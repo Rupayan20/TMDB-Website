9b40af43ba37cfdd6d70573847e098cc4293aead
Rupayan Dutta <102848153+Rupayan20@users.noreply.github.com>
2023-01-03 00:11:53 +0530

    Create Notes.jsx

---
+++ src/components/Notes.jsx
@@ -0,0 +1,83 @@
+ .navbar {
+     background-color: rgb(3, 37, 65);
+     color: white;
+     height: 60px;
+     position: fixed;
+     width: 100%;
+     z-index: 1;
+ }
+
+ .navbar .container {
+     display: flex;
+     flex-direction: row;
+     align-items: center;
+     justify-content: space-between;
+ }
+
+ .navbar .container .logo {
+     font-family: 'Raleway', sans-serif;
+     font-size: 30px;
+     letter-spacing: 2px;
+ }
+
+ 
+ .navbar .links ul {
+     list-style-type: none;
+     display: flex;
+     flex-direction: row;
+     justify-content: space-between;
+     width: 400px;
+     position: relative;
+    top: 7px;
+     
+ }
+ .navbar .links ul li a span:nth-child(2){
+    display: none;
+ }
+ @media screen and (max-width:600px) {
+
+    .navbar .links ul li a span:nth-child(2){
+        display: inline-block;
+     }
+     .navbar .links ul li a span:nth-child(1){
+        display: none;
+     }
+     .navbar .links ul {
+
+        width: 200px;
+        position: relative;
+       top: 12px;
+        
+    }
+    
+ }
+ .navbar .links ul a{
+    font-family: 'Raleway', sans-serif;
+     font-size: 20px;
+     
+ }
+  #moviesType{
+   
+   
+   display: flex;
+   flex-direction: column;
+   width: 100%;
+   background-color: white;
+   color: black;
+   position: absolute;
+   top: 30px;
+   width: 180px;
+   padding-top: 10px;
+   padding-bottom: 10px;
+   border-radius: 10px;
+   display: none;
+   
+
+ }
+ #moviesType a{
+   color: black;
+   font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+ }
+ #movieType:hover #moviesType{
+     display: block;
+ }
